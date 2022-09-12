22a7ef8e61f659e765925b68aa856b272f154302
obisco2 <obitadeoladayo@gmail.com>
2022-09-12 12:21:13 -0700

    4puts

---
+++ 0x00-hello_world/4puts.c
@@ -0,0 +1,6 @@
+/**                                                                                                                                                                     * main - print the string the strings in the c function                                                                                                                 * description: using hte main function                                                                                                                                  * this programming prints "ramming is like building a multilingual puzzle"                                                                                              */
+int main (void)
+{
+     puts("/"Programming is like building a multilingual puzzle");
+     return (0)
+}
