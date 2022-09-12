8e9230dacdabff593eec453ea936bd80d0f5cad9
obisco2 <obitadeoladayo@gmail.com>
2022-09-12 15:23:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-/**                                                                                                                                                                     * main - print the string the strings in the c function                                                                                                                 * description: using hte main function                                                                                                                                  * this programming prints "ramming is like building a multilingual puzzle"                                                                                              */                                                                                                                                                                      int main (void)                                                                                                                                                         {                                                                                                                                                                            puts("/"Programming is like building a multilingual puzzle");                                                                                                           return (0);
-}	
+#!/bin/bash
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Programming is like building a multilingual puzzle");
+   return 0;
+}
