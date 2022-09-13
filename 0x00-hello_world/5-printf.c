ebd32ec03e8505e829b46e0755a0e0b631452c0e
obisco2 <obitadeoladayo@gmail.com>
2022-09-13 00:10:59 -0700

    coding2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,23 @@
-#include <stdio.h>
+#!/bin/bash
+#include <stdio.h
 
 /**
-* This is the preferred style for multi-line
-*
-* Description:  A column of asterisks on the left side,
-* with beginning and ending almost-blank lines.
-* return 0
-*/
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
 int main(void)
+
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
+
 }
+
