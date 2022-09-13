5d80e27f7daa0e18cbd9831411b801457985a1d7
obisco2 <obitadeoladayo@gmail.com>
2022-09-12 17:37:23 -0700

    coding2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
-
-
-
 /**
-
-	 * This is the preferred style for multi-line
-
-	 * comments in C source code.
-
-	 * Please use it consistently.
-
-	 *
-
-	 * Description:  A column of asterisks on the left side,
-
-	 * with beginning and ending almost-blank lines.
-
-	 */
-
+* This is the preferred style for multi-line
+* comments in C source code.
+* Please use it consistently.
+*
+* Description:  A column of asterisks on the left side,
+* with beginning and ending almost-blank lines.
+*/
 int main(void)
-
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
-
 }
